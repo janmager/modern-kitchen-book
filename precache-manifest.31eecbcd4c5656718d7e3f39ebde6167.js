97ef2ae9a08dfa2aafc405180ab265ba1e524809
janmager <jasiu.mager@gmail.com>
2020-05-08 00:21:37 +0200

    Updates

---
+++ precache-manifest.31eecbcd4c5656718d7e3f39ebde6167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95dc1650697bfb822e37ece77a44214",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "ccdf3e697d57e506d473",
+    "url": "/recipe-app/static/css/main.0d2bd5f6.chunk.css"
+  },
+  {
+    "revision": "2dc248abb78a41aecfe0",
+    "url": "/recipe-app/static/js/2.867f6d07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-app/static/js/2.867f6d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccdf3e697d57e506d473",
+    "url": "/recipe-app/static/js/main.64fe869e.chunk.js"
+  },
+  {
+    "revision": "c0ff539ea545a33668e1",
+    "url": "/recipe-app/static/js/runtime-main.c350eb4f.js"
+  }
+]);
